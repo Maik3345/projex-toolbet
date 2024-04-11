aee626622db4911e6a74d9a1b97a650eed6a66cd
Michael Stiven Restrepo Alvarez <michael.restrepo@puntoscolombia.com>
2024-04-11 07:43:16 -0500

    main: fix: :bug: Update PR template with new gif link

--- src/shared/utils/code/git/setup/devops/templates/base.ts
+++ src/shared/utils/code/git/setup/devops/templates/base.ts
@@ -24,24 +24,4 @@ export const DEVOPS_TEMPLATE_CODE = `#### What problem is this solving?
 
 <!-- Go to http://giphy.com/ and pick a gif that represents how this PR makes you feel -->
 
-![](put .gif link here - can be found under "advanced" on giphy)
-
-### Select the release type of this PR
-
-Please select which version do you want to release:
-
-- [ ] Patch (backwards-compatible bug fixes)
-
-- [ ] Minor (backwards-compatible functionality)
-
-- [ ] Major (incompatible API changes)
-
-And then you just need to merge your PR when you are ready! There is **no need** to create a release commit/tag.
-
-- [ ] No thanks, I would rather do it manually 😞
-
-### Make a beta build
-
-Please indicate if you want to make a beta build and deploy
-
-- [ ] Yes, I want to make a beta build and deploy it`;
+![](put .gif link here - can be found under "advanced" on giphy)`;
